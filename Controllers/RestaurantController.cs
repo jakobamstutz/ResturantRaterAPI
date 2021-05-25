aee50ef96e792a05005124a6c769de4e75a0c7ea
Jakob Amstutz <amstutzjakob@gmail.com>
2021-05-25 12:11:42 -0400

    second commit

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,33 @@
+﻿using ResturantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+
+namespace ResturantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+
+    {
+        private RestaurantDbContext _context = new RestaurantDbContext();
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if (model == null)
+            {
+                return BadRequest("Your request body can't be empty");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
